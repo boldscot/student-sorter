17a1cd6c5e7c01821c2653acbc8bf13327d84aed
boldscot <bold-scot@hotmail.com>
2019-02-12 20:25:42 +0000

    1st commit, initializing repo

---
+++ src/FileReader.java
@@ -0,0 +1,5 @@
+import java.io.File;
+import org.json.JSONObject;
+
+public class FileReader {
+}
---
+++ src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+	
+	private String name;
+	private float grade;
+	
+	public Student (String _name, float _grade) {
+		this.name = _name;
+		this.grade = _grade;
+	}
+	
+	public String getName () {
+		return this.name;
+	}
+	
+	public float getGrade () {
+		return this.grade;
+	}
+	
+}
